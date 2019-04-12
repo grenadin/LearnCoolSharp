7d55caef840b556f8b1e91cb572539256f621937
HARIRAK SRITHAM <harirak@hotmail.co.th>
2019-04-13 03:31:54 +0700

    Add project files.

---
+++ LearnCoolSharp/CH07/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnCoolSharp.CH07
+{
+    class Person
+    {
+        public string name, surname;
+        public bool gender;
+        public int age;
+        public string nationality;
+
+        public void Walk()
+        {
+            Console.WriteLine($"Walk");
+        }
+        public void Run()
+        {
+            Console.WriteLine($"Run...");
+        }
+        public void Talk(string sentence)
+        {
+            Console.WriteLine($"Say...{sentence}");
+        }
+
+
+    }
+}
---
+++ LearnCoolSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using LearnCoolSharp.CH07;
+
+namespace LearnCoolSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person me = new Person();
+            me.name = "HARIRAK";
+            me.surname = "SRITHAM";
+            me.age = 41;
+            me.Talk("oh");
+            Console.WriteLine($"{me.age}");
+
+            
+            
+            
+        }
+    }
+}
